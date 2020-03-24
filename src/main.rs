6f9dbb486700e2595fd9d4a04d238c1b5b5fde56
Kazuhei Hamada <kazuhei.git@gmail.com>
2020-03-25 01:03:18 +0900

    Receive fifo works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate nix;
+extern crate tempfile;
+
+use nix::sys::stat;
+use nix::unistd;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::path::Path;
+use std::thread::sleep;
+use std::time;
+
 fn main() {
-    println!("Hello, world!");
+    let fifo_path = Path::new("/tmp/foo.pipe");
+    let mut file = OpenOptions::new().read(true).open(&fifo_path).unwrap();
+    println!("Opened a file");
+    let mut contents = String::new();
+    loop {
+        println!("Begin loop");
+        file.read_to_string(&mut contents).unwrap();
+        println!("Received: {}", contents);
+        sleep(time::Duration::from_secs(3));
+    }
 }
